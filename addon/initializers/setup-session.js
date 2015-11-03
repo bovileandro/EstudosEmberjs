ab4fa78b33402fbd3ecca152e8376116d96fde8c
unknown <lenita@WS033.Processa.com>
2015-11-03 09:42:47 -0200

    ajuste no inject da sessão

--- addon/initializers/setup-session.js
+++ addon/initializers/setup-session.js
@@ -1,8 +1,6 @@
-import Ember from 'ember';
-
-export function initialize(application) {
-  //application.inject('controller', 'session', 'service:session');
-  //application.inject('route', 'session', 'service:session');
+export function initialize( container, application ) {
+  application.inject('controller', 'session', 'service:session');
+  application.inject('route', 'session', 'service:session');
 }
 
 export default {
